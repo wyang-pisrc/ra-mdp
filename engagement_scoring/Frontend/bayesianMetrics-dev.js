97380e54b5945ff3804bf9e5a60b091c84d8bd07
Wei Yang <wyang@pisrc.com>
2023-02-09 11:47:39 -0500

    Finetune JS for edge cases

--- engagement_scoring/Frontend/bayesianMetrics-dev.js
+++ engagement_scoring/Frontend/bayesianMetrics-dev.js
@@ -90,21 +90,35 @@ function showPiSightStorage() {
     showPiSightProfile();
 }
 
-function loadPreviousParts(mcvisid) {
-    var piSight = window.localStorage.getItem(PISIGHT_LOCAL_STORAGE_NAME); // document.cookie.match(new RegExp(name + "=([^;]+)"));
+function hasPreviousParts() {
+    if (typeof Storage !== "undefined") {
+        if (window.localStorage.getItem(PISIGHT_LOCAL_STORAGE_NAME)) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        console.log("browser is not supporting local storage for Pisight");
+        return false;
+    }
+}
+
+function loadPreviousParts(aspectName, mcvisid) {
+    var piSight = window.localStorage.getItem(PISIGHT_LOCAL_STORAGE_NAME);
 
     if (piSight) {
-        userMetrics = JSON.parse(piSight);
-    } else if (mcvisid) {
-        userMetrics = INIT_METRICS;
-    } else {
-        userMetrics = INIT_METRICS;
+        userMetrics = JSON.parse(piSight)[aspectName];
+    }
+
+    if (mcvisid) {
+        // TODO: if user visited records upload to cloud
+        // userMetrics = INIT_METRICS[aspectName];
     }
     return userMetrics;
 }
 
-function calcOneAspectBayesianProbability(updatedParts, aspect_proportion) {
-    let aspectLabels = Object.keys(aspect_proportion);
+function calcOneAspectBayesianProbability(updatedParts, aspectProportion) {
+    let aspectLabels = Object.keys(aspectProportion);
     var numerators = {};
     var denominator = DECIMAL_ZERO;
     const isSorted = true;
@@ -112,7 +126,7 @@ function calcOneAspectBayesianProbability(updatedParts, aspect_proportion) {
     // no-log multiply version
     for (let label of aspectLabels) {
         let part = new BigDecimal(updatedParts[label]);
-        let proportion = new BigDecimal(aspect_proportion[label]);
+        let proportion = new BigDecimal(aspectProportion[label]);
         let products;
         if (IS_LOG_VERSION) {
             products = Math.exp(part.add(proportion)); // it will be really small number as well
@@ -120,9 +134,7 @@ function calcOneAspectBayesianProbability(updatedParts, aspect_proportion) {
             products = part.multiply(proportion);
         }
 
-        numerators = Object.assign(numerators, {
-            [label]: products,
-        });
+        numerators = { ...numerators, [label]: products };
         denominator = denominator.add(products);
     }
 
@@ -130,12 +142,9 @@ function calcOneAspectBayesianProbability(updatedParts, aspect_proportion) {
 
     for (let label of aspectLabels) {
         let labelProb = numerators[label].divide(denominator.equal(DECIMAL_ZERO) ? "1.0" : denominator).toString();
-        probability = Object.assign(probability, {
-            [label]: labelProb,
-        });
+        probability = { ...probability, [label]: labelProb };
     }
 
-    // sorted
     if (isSorted) {
         probability = Object.entries(probability).sort((a, b) => b[1] - a[1]);
     }
@@ -143,61 +152,53 @@ function calcOneAspectBayesianProbability(updatedParts, aspect_proportion) {
     return [probability, flag];
 }
 
-function getOneAspectParts(aspect_kYields, aspect_proportion, traffic) {
-    let aspectLabels = Object.keys(aspect_proportion);
+function calcOneAspectParts(aspectKYields, aspectProportion, traffic) {
+    let aspectLabels = Object.keys(aspectProportion);
     var parts = {};
     for (let label of aspectLabels) {
-        let kYield = new BigDecimal(aspect_proportion[label]);
-        let proportion = new BigDecimal(aspect_kYields[label]);
+        let kYield = new BigDecimal(aspectProportion[label]);
+        let proportion = new BigDecimal(aspectKYields[label]);
         let sharedTraffic = new BigDecimal(traffic);
         conditionalProbability = kYield.multiply(sharedTraffic).divide(proportion.equal(DECIMAL_ZERO) ? "1.0" : proportion);
-        parts = Object.assign(parts, {
-            [label]: conditionalProbability.toString(),
-        });
+        parts = { ...parts, [label]: conditionalProbability.toString() };
     }
     return parts;
 }
 
 // multiply with previous records
-function updateCulmulativeParts(previousParts, currentAspectParts, currentAspectName) {
-    if (!isConsistentAspect(previousParts) || !previousParts.hasOwnProperty(currentAspectName)) {
-        console.log("previousParts: ", previousParts);
-        console.log("currentAspectName: ", currentAspectName);
-        console.log("currentAspectParts: ", currentAspectParts);
-        throw new Error("Data is not consistent when cumulating");
-    }
-
-    var previousAspectParts = previousParts[currentAspectName];
-    // console.log("previousAspectParts", previousAspectParts);
+function calcOneCulmulativeParts(previousAspectParts, currentAspectParts) {
     var culmulativeParts = {};
     if (IS_LOG_VERSION) {
         // log sum version
         for (let label of Object.keys(previousAspectParts)) {
             let previous = parseFloat(previousAspectParts[label]);
             let current = parseFloat(currentAspectParts[label]);
-            culmulativeParts = Object.assign(culmulativeParts, {
-                [label]: (previous + current).toPrecision(50),
-            });
+            culmulativeParts = { ...culmulativeParts, [label]: (previous + current).toPrecision(50) };
         }
     } else {
         // no-log multiply version
         for (let label of Object.keys(previousAspectParts)) {
             let previous = new BigDecimal(previousAspectParts[label]);
             let current = new BigDecimal(currentAspectParts[label]);
-            culmulativeParts = Object.assign(culmulativeParts, {
-                [label]: previous.multiply(current).toString(),
-            });
+            culmulativeParts = { ...culmulativeParts, [label]: previous.multiply(current).toString() };
         }
     }
 
     return culmulativeParts;
 }
 
+function getOneAspectMetrics(aspectName, allPageMetrics) {
+    let aspectKYields = Object.fromEntries(Object.entries(allPageMetrics["kYieldModified"]).filter(([key]) => key.includes(aspectName)));
+    let aspectProportion = Object.fromEntries(Object.entries(allPageMetrics["labelProportion"]).filter(([key]) => key.includes(aspectName)));
+    return [aspectKYields, aspectProportion];
+}
+
 // store the current culmulative result
 function storeCulmulativeParts(updatedParts) {
     if (!isConsistentAspect(updatedParts)) {
         console.log("updatedParts: ", updatedParts);
-        throw new Error("Data is not consistent when uploading");
+        console.log("Data is not consistent when storing, reset local storage");
+        resetPiSightStorage();
     }
 
     window.localStorage.setItem(PISIGHT_LOCAL_STORAGE_NAME, JSON.stringify(updatedParts));
@@ -215,7 +216,7 @@ function showPiSightProfile() {
     console.log("current user profile: ", JSON.parse(window.localStorage.getItem(PISIGHT_PROFILE_LOCAL_STORAGE_NAME)));
 }
 
-function identifySignal(profiles) {
+function identifySignal(userProfiles) {
     // based on metrics, what kind of user need to be flag
     return true;
 }
@@ -267,102 +268,65 @@ async function piSightMain() {
         return;
     }
 
-    var currentMetrics = await fetchPageMetrics(corePath).then((data) => data);
-    var traffic = currentMetrics["traffic"];
+    var allPageMetrics = await fetchPageMetrics(corePath).then((data) => data);
+    if (allPageMetrics == undefined) {
+        console.log("pageMetrics Endpoint is not ready.");
+        return;
+    }
+
+    var traffic = allPageMetrics["traffic"];
     if (traffic == undefined) {
         console.log("no valid records for this page URL yet.");
         return;
     }
-    var aspects = ASPECTS;
 
-    var previousParts = loadPreviousParts();
-    var result = {};
+    var userMetrics = {};
     var uploadParts = {};
     var flags = {};
-    for (let aspectName of aspects) {
-        let aspect_kYields = Object.fromEntries(Object.entries(currentMetrics["kYieldModified"]).filter(([key]) => key.includes(aspectName)));
-        let aspect_proportion = Object.fromEntries(Object.entries(currentMetrics["labelProportion"]).filter(([key]) => key.includes(aspectName)));
-        let currentAspectParts = getOneAspectParts(aspect_kYields, aspect_proportion, traffic);
-        updatedParts = updateCulmulativeParts(previousParts, currentAspectParts, aspectName);
-        let [probability, flag] = calcOneAspectBayesianProbability(updatedParts, aspect_proportion);
+    for (let aspectName of ASPECTS) {
+        let [aspectKYields, aspectProportion] = getOneAspectMetrics(aspectName, allPageMetrics);
+        let currentAspectParts = calcOneAspectParts(aspectKYields, aspectProportion, traffic);
 
-        result = Object.assign(result, {
-            [aspectName]: probability,
-        });
-        uploadParts = Object.assign(uploadParts, {
-            [aspectName]: updatedParts,
-        });
-        flags = Object.assign(flags, {
-            [aspectName]: flag,
-        });
+        if (hasPreviousParts()) {
+            previousAspectParts = loadPreviousParts(aspectName, aspectKYields);
+            updatedParts = calcOneCulmulativeParts(previousAspectParts, currentAspectParts);
+        } else {
+            updatedParts = currentAspectParts;
+        }
+
+        let [probability, flag] = calcOneAspectBayesianProbability(updatedParts, aspectProportion);
+
+        uploadParts = { ...uploadParts, [aspectName]: updatedParts };
+        userMetrics = { ...userMetrics, [aspectName]: probability };
+        flags = { ...flags, [aspectName]: flag };
     }
 
-    var profiles = {
-        userMetrics: result,
+    var userProfiles = {
+        userMetrics: userMetrics,
         potentialFlags: flags,
-        pageMetrics: currentMetrics["kYieldModified"],
-        pageTraffic: currentMetrics["traffic"],
     };
 
-    // Verbose only
-    for (let aspect of Object.keys(profiles["potentialFlags"])) {
-        console.log("highest prob in aspect:", aspect);
-        console.log(profiles["potentialFlags"][aspect]);
-    }
-    // console.log("profiles[potentialFlags] : ", profiles["potentialFlags"]);
-    // console.log("profiles[userMetrics] : ", profiles["userMetrics"]);
-    // console.log("uploadParts[lead]: ", uploadParts["lead"]);
-    storePiSightProfile(profiles);
+    storePiSightProfile(userProfiles);
     storeCulmulativeParts(uploadParts);
-}
 
-// for serving cold start user. Assume they visit the en-us homepage
-const INIT_METRICS = {
-    lead: { "lead-Good": "0.332185769665836405639680606327601708471775054931640625", "lead-Bad": "0.64247641634327001813886681702570058405399322509765625" },
-    role: {
-        "role-Csuite": "0.0258060603484974900034831790662792627699673175811767578125",
-        "role-Manager": "0.2012114001622929138068940346784074790775775909423828125",
-        "role-Engineer": "0.2323283994299165933217210522343521006405353546142578125",
-        "role-Other": "0.5087378156955304486785962581052444875240325927734375",
-    },
-    industry: {
-        "industry-Aerospace": "0.003878987921129240130924120677491373498924076557159423828125",
-        "industry-Infrastructure": "0.0002424367450705775081827575423432108436827547848224639892578125",
-        "industry-Automotive_Tire": "0.070549092815538061440605588359176181256771087646484375",
-        "industry-Cement": "0.050184406229609547500647437345833168365061283111572265625",
-        "industry-Chemical": "0.0089701595676113694832753964192306739278137683868408203125",
-        "industry-Entertainment": "0",
-        "industry-Fibers_Textiles": "0.002666804195776352644220441590050540980882942676544189453125",
-        "industry-Food_Beverage": "0.2802568773015876590903872056514956057071685791015625",
-        "industry-Glass": "0.000484873490141155016365515084686421687365509569644927978515625",
-        "industry-HVAC": "0",
-        "industry-Household_Personal_Care": "0.030789466623963347713388571946779848076403141021728515625",
-        "industry-Life_Sciences": "0.00121218372535288770354411358454171931953169405460357666015625",
-        "industry-Marine": "0.00751553909718790345839689592821741825900971889495849609375",
-        "industry-Metals": "0.055033141131021097447462153695596498437225818634033203125",
-        "industry-Mining": "0.01042478003803483377343042093343683518469333648681640625",
-        "industry-Oil_Gas": "0.1069146045761246999195037687968579120934009552001953125",
-        "industry-Power_Generation": "0.00096974698028231003273103016937284337473101913928985595703125",
-        "industry-Print_Publishing": "0.003151677685917507985846608420388292870484292507171630859375",
-        "industry-Pulp_Paper": "0.0654579211690559381597864785362617112696170806884765625",
-        "industry-Semiconductor": "0.00969746980282310162835290867633375455625355243682861328125",
-        "industry-Whs_EComm_Dist": "0",
-        "industry-Waste_Management": "0",
-        "industry-Water_Wastewater": "0.0305470298788927674404902745664003305137157440185546875",
-        "industry-Other": "0.255528329304388679421577990069636143743991851806640625",
-    },
-};
+    if (VERBOSE) {
+        for (let aspect of Object.keys(userProfiles["potentialFlags"])) {
+            console.log(`highest prob in ${aspect} aspect:`);
+            console.log(userProfiles["potentialFlags"][aspect]);
+        }
+        // console.log("userProfiles[potentialFlags] : ", userProfiles["potentialFlags"]);
+        // console.log("userProfiles[userMetrics] : ", userProfiles["userMetrics"]);
+        // console.log("uploadParts[lead]: ", uploadParts["lead"]);
+    }
+}
 
 const DECIMAL_ZERO = new BigDecimal("0.0");
 const ASPECTS = ["lead", "role", "industry"];
-
 const SERVLET_PATH = window.location.origin + "/bin/rockwell-automation/content-score";
-// const SERVLET_PATH = "https://dev-aem.rockwellautomation.com/bin/rockwell-automation/content-score";
-// const SERVLET_PATH = "https://qa-aem.rockwellautomation.com/bin/rockwell-automation/content-score";
 const PISIGHT_LOCAL_STORAGE_NAME = "piSight";
 const PISIGHT_VISITS_LOCAL_STORAGE_NAME = "piSightVisitedPages";
 const PISIGHT_PROFILE_LOCAL_STORAGE_NAME = "piSightProfile";
 const IS_LOG_VERSION = false;
-
-resetPiSightStorage();
+const VERBOSE = true;
+// resetPiSightStorage();
 await piSightMain();
